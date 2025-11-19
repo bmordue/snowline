74fd27020cd9d0e6590a8778305ef1fd4a438ac9
Ben Mordue <benmordue@gmail.com>
2025-11-19 13:04:43 +0000

    initial flake

---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+  description = "A development environment for the Snowline Visualization Tool";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }: let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs {
+      inherit system;
+    };
+  in {
+    devShells.${system}.default = pkgs.mkShell {
+      name = "snowline-env";
+      buildInputs = with pkgs; [
+        (python3.withPackages (ps: with ps; [
+          geopandas
+          rasterio
+          matplotlib
+          cartopy
+          pyyaml
+          shapely
+          numpy
+        ]))
+        # Add other system dependencies here if needed
+      ];
+
+      shellHook = ''
+        echo "Entering Snowline Visualization Tool development environment..."
+        export PYTHONPATH=$(pwd)
+      '';
+    };
+  };
+}
